4c50d4e7317aa6166df277c4f252bf1f84db0f64
Sycamore <pierre.chassagne@epfl.ch>
2022-08-11 17:26:53 +0200

    Debug + minor modifs

--- config/init_mocap.sh
+++ config/init_mocap.sh
@@ -4,4 +4,4 @@ source /opt/ros/noetic/setup.bash
 export ROS_IP=192.168.1.9
 export ROS_HOSTNAME=192.168.1.9
 export ROS_MASTER_URI=http://192.168.1.9:11311
-roslaunch vrpn_client_ros sample.launch server:=192.168.1.11 
+roslaunch mocap_optitrack mocap.launch mocap_config_file:=config/mocap_config.yaml
